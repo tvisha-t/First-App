9543e38537fef5c24e2df2a5c6349eb802a00ae8
tvisha-t <incredibletvisha101@gmail.com>
2023-06-26 13:09:15 -0500

    Initial Commit

---
+++ First App/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  First App
+//
+//  Created by Deepali Chawla on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        VStack {
+            Text("Heres a  cat")
+                .font(.title)
+                .foregroundColor(Color.blue)
+            Image("cutecat")
+       
+        }
+        .padding(.all)
+        
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Deepali Chawla on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
